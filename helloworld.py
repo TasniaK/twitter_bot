afe4a52689276e1f726924dc754a08ae8b7594e0
Tasnia <tasniakamal@hotmail.co.uk>
2018-06-20 13:26:47 +0100

    Added files to project, set up twitter app linked to my twitter username. Authenticated twitter_bot to use the twitter app through Tweepy library

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+import tweepy
+import os
+
+consumer_key = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access_token = os.environ['ACCESS_TOKEN']
+access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+tweet = 'Hello, world!'
+api.update_status(status=tweet)
