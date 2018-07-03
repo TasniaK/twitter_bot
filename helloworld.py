70fdf5af5eaba5a24d717654bbeea07e7edfb05e
Tasnia <tasniakamal@hotmail.co.uk>
2018-07-03 12:40:06 +0100

    Started with common search query 'bunny OR muffin' where results get written to a csv file

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 import tweepy
 import os
+import csv
 
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
@@ -11,5 +12,21 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-tweet = 'Hello, world!'
-api.update_status(status=tweet)
+# tweet = 'Hello, world!'
+# api.update_status(status=tweet)
+
+# query = 'OA OR Oblivion Allstars OR Unity OR QMA OR Sparks OR Ascension Eagles OR AE OR Surrey Starlets OR Eclipse OR Royals OR Marshals OR Coventry Dynamite'
+query = 'bunny OR muffin'
+max_tweets = 100
+
+results = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
+
+# create csv file for data to go into
+with open('cheer_teams.csv', 'a') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    # write tweets to csv file
+    for tweet in results:
+        filewriter.writerow([tweet.text.encode('utf-8')])
+        print(tweet)
